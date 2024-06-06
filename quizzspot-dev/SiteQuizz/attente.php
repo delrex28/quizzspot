f708b6c63a95651b26d04381b269acb5e6773acb
cryo24 <163122434+cryo24@users.noreply.github.com>
2024-06-06 13:22:36 +0200

    Update attente.php

--- quizzspot-dev/SiteQuizz/attente.php
+++ quizzspot-dev/SiteQuizz/attente.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,66 +9,118 @@
     <link rel="stylesheet" type="text/css" href="css/styles.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
+
 <body>
     <script>
-        // Fonction pour vérifier le statut du quizz
-        function checkQuizzStatus() {
-            $.ajax({
-                url: 'http://localhost/SiteQuizz/debut_quizz.json', //A terme, l'api enverra true si le quizz est marqué comme commencé dans la BDD
-                method: 'GET',
-                success: function(response) {
-                    if (response.quizz_debut == true) {
-                        $('#status').text('Le quizz a commencé !');
-                        // Rediriger l'utilisateur vers la page du quizz
-                        window.location.href = 'question.php';
-                    } else {
-                        $('#status').text('En attente du début du quizz...');
+        $(document).ready(function () {
+            // Récupérer les paramètres de l'URL
+            const urlParams = new URLSearchParams(window.location.search);
+            const nomcomplet = urlParams.get('nomcomplet');
+
+            if (!nomcomplet) {
+                console.error('Nom complet manquant.');
+                return;
+            }
+
+            // Séparer la chaîne en utilisant l'espace comme délimiteur
+            let nameParts = nomcomplet.split(" ");
+            let prenom = nameParts[0];
+            let nom = nameParts[1];
+
+            if (!prenom || !nom) {
+                console.error('Prénom ou nom manquant.');
+                return;
+            }
+
+            // Fonction pour vérifier le statut du quizz
+            function checkQuizzStatus() {
+                $.ajax({
+                    url: 'https://app.quizzspot.fr/is_quizz_started',
+                    method: 'GET',
+                    success: function (response) {
+                        console.log(response.msg);
+                        if (response.msg === true) {
+                            $('#status').text('Le quizz a commencé !');
+                            // Rediriger l'utilisateur vers la page du quizz
+                            window.location.href = 'question.php';
+                        } else {
+                            $('#status').text('En attente du début du quizz...');
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        console.error('Erreur lors de la vérification du statut du quizz : ' + error);
                     }
-                },  
+                });
+            }
+
+            setInterval(checkQuizzStatus, 2000); // Vérifier toutes les 2 secondes
+
+            // Fonction pour remettre l'apprenant en disponible via l'API
+            function setAvailable(nom, prenom) {
+                $.ajax({
+                    url: 'https://app.quizzspot.fr/unmark_connected',
+                    method: 'GET',
+                    data: {
+                        nom: encodeURIComponent(nom),
+                        prenom: encodeURIComponent(prenom)
+                    },
+                    success: function (response) {
+                        console.log("OSKOUR")
+                        // window.location.href = 'connexion.html';
+                    },
+                    error: function (xhr, status, error) {
+                        console.error('Erreur lors de la mise à jour de la disponibilité de l\'apprenant : ' + error);
+                    }
+                });
+            }
+
+            // Ajouter l'événement pour remettre l'apprenant en disponible
+            $('#setAvailableLink').on('click', function () {
+                setAvailable(nom, prenom);
             });
-        }
-        
 
-        setInterval(checkQuizzStatus, 5000); // Vérifier toutes les 5 secondes
-
-
-        // Fonction pour insérer le token dans la base de données via l'API
-        function insertToken(token, nomcomplet) {
-            $.ajax({
-                url: 'insert_token.php', // URL de l'API pour l'insertion du token
-                method: 'POST',
-                data: { token: token, nomcomplet: nomcomplet },
-                success: function(response) {
-                    console.log('Token inséré avec succès dans la base de données.');
-                },
-                error: function(xhr, status, error) {
-                    console.error('Erreur lors de l\'insertion du token dans la base de données : ' + error);
-                }
-            });
-        }
+        });
     </script>
     <div class="container">
         <h1>Bienvenue !</h1>
         <?php
-        // Vérifier si un nom a été sélectionné
+        // Vérifier si un nom complet a été sélectionné
         if (isset($_GET['nomcomplet'])) {
-            // Récupérer le nom sélectionné
-            $selected_name = $_GET['nomcomplet'];
-            echo "<p>Connecté en tant que : $selected_name</p>";
+            // Récupérer le nom complet sélectionné
+            $nomcomplet = $_GET['nomcomplet'];
+            echo "<p>Connecté en tant que : <b>$nomcomplet</b></p>";
             $token = uniqid();
-            setcookie("token", $token, time() + (7200), "/"); //Token dans cookie Valide pendant 2h
-            // Appeler le script pour insérer le token dans la base de données
-            echo "<script>insertToken('$token', '$selected_name')</script>";
+            setcookie("token", $token, time() + (7200), "/"); // Token dans cookie valide pendant 2h
             echo "<p>Le Quizz commencera sous peu.</p>";
-            echo "<p>DEBUG :  VOTRE TOKEN EST $token";
-            echo "<p>Vous n'êtes pas $selected_name ? <a href='connexion.html'>Retour</a></p>";
-            #Ne pas oublier de rajouter une requete pour remettre le nom comme dispo
+            echo "<p>DEBUG : VOTRE TOKEN EST $token</p>";
+            echo "<p>Vous n'êtes pas $nomcomplet ? <a href='connexion.html' id='setAvailableLink'>Retour</a></p>";
         } else {
-            echo "<p>Vous n'êtes pas connecté. <a href='selection.php'>Sélectionnez votre nom</a></p>";
+            echo "<p>Vous n'êtes pas connecté. <a href='connexion.html'>Sélectionnez votre nom</a></p>";
         }
         ?>
         <p id="status">En attente du début du quizz...</p>
     </div>
-    
+
+    <script>
+        const urlParams = new URLSearchParams(window.location.search);
+        const nomcomplet = urlParams.get('nomcomplet');
+        const token = <?php echo json_encode($token); ?>;
+        console.log(token);
+        console.log(nomcomplet);
+        $.ajax({
+                    url: 'https://app.quizzspot.fr/insert_token', // URL de l'API pour l'insertion du token
+                    method: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify({ token: token, nom_complet: nomcomplet }),
+                    success: function(response) {
+                        console.log('Token inséré avec succès dans la base de données.');
+                    },
+                    error: function(xhr, status, error) {
+                        console.error('Erreur lors de l\'insertion du token dans la base de données : ' + error);
+                    }
+                });
+    </script>
+
 </body>
+
 </html>
